7c51152e13a852ad2820535e41c7c1e6b8511f88
alanamooraes <alanamoraes31@hotmail.com>
2022-05-14 19:33:13 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,6 +9,8 @@ public class PrimeiroPrograma {
 
         /*int a = 5;
         int b = 3;
+        System.out.println("Hello World!" + (a+b));*//*int a = 5;
+        int b = 3;
         System.out.println("Hello World!" + (a+b));*/
     }
 
